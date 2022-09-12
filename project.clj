268261cd758c8bbb07752323fe7c5c16d530ac7e
ganesh <ganesh@zigiwave.com>
2022-09-12 21:09:01 +0530

    initial commit

---
+++ project.clj
@@ -0,0 +1,24 @@
+(defproject cheffy "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [ring "1.9.6"]
+                 [integrant "0.8.0"]
+                 [environ "1.2.0"]
+                 [metosin/reitit "0.5.18"]
+                 [seancorfield/next.jdbc "1.2.659"]
+                 [org.postgresql/postgresql "42.2.14"]
+                 [clj-http "3.10.0"]
+                 [ovotech/ring-jwt "1.3.0"]]
+  :main ^:skip-aot cheffy.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:source-paths ["dev/src"]
+                   :resource-paths ["dev/resources"]
+                   :dependencies [[ring/ring-mock "0.4.0"]
+                                  [integrant/repl "0.3.2"]]}}
+  :uberjar-name "cheffy.jar")
